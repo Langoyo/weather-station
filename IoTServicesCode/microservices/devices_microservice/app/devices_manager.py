64f5149adf14511406f27751d51f65117bbe6f26
ANDRES LANGOYO MARTIN <100405869@alumnos.uc3m.es>
2021-05-22 19:58:37 +0200

    testing message router

--- IoTServicesCode/microservices/devices_microservice/app/devices_manager.py
+++ IoTServicesCode/microservices/devices_microservice/app/devices_manager.py
@@ -57,7 +57,7 @@ def devices_register(params):
     mydb=connect_database()
     with mydb.cursor() as mycursor:
 
-        if check_existing_device(params["device"]):
+        if True or check_existing_device(params["device"]):
             sql= "UPDATE devices SET status=%s, timestamp=%s WHERE device_id = %s"
             val=('Active',params["timestamp"] ,params["device"])
             try:
