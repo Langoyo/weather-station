8ebb1624164c6be386f3c5d74b2d6227adb374b0
ANDRES LANGOYO MARTIN <100405869@alumnos.uc3m.es>
2021-05-28 18:49:04 +0200

    removing unused files

--- RaspberrypiCode/weather_station.py
+++ RaspberrypiCode/weather_station.py
@@ -51,7 +51,7 @@ def temperatureSensor():
         if temperature is not None:
             if newtemperature != temperature:
                 newtemperature = temperature
-                # pub.send_temperature(newtemperature, now)
+                pub.send_temperature(newtemperature, now)
                 temperature_message = "Temp={0:0.1f}C".format(newtemperature)
             print(temperature_message)
         else:
@@ -74,7 +74,7 @@ def humiditySensor():
         if humidity is not None:
             if newhumidity != humidity:
                 newhumidity = humidity
-                # pub.send_humidity(newhumidity, now)
+                pub.send_humidity(newhumidity, now)
                 humidity_message = "Humidity={}%".format(newhumidity)
             print("Humidity={}%".format(newhumidity))
         else:
@@ -105,14 +105,19 @@ def button_handler(channel):
 
 def weatherSensor():
     now = datetime.datetime.today().replace(microsecond=0)
-    # pub.make_connection()
+    pub.make_connection()
     id = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
                    for ele in range(0, 8 * 6, 8)][::-1])
     print(id)
     pub.send_id("Device:" + id, now)
 
 
-# def locationSensor():
+def locationSensor():
+    while True:
+        now = datetime.datetime.today().replace(microsecond=0)
+        location = "Should be location"
+        pub.send_location(location, now)
+        time.sleep(3600)
 #     while True:
 #         received_data = (str)(ser.readline())  # read NMEA string received
 #         GPGGA_data_available = received_data.find(
@@ -155,11 +160,12 @@ if __name__ == '__main__':
     GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING,
                           callback=button_handler, bouncetime=100)
 
-    # location_thread = threading.Thread(target=locationSensor)
+    location_thread = threading.Thread(target=locationSensor)
     humidity_thread = threading.Thread(target=humiditySensor)
     temperature_thread = threading.Thread(target=temperatureSensor)
     display_thread = threading.Thread(target=showDataOnDisplay)
-    # location_thread.start()
+
+    location_thread.start()
     temperature_thread.start()
     humidity_thread.start()
     display_thread.start()
